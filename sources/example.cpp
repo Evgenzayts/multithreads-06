dddca85932f1ae35116eab86ce8104f09ab10f3a
Evgenzayts <evgenzaytsev2002@yandex.ru>
2022-05-15 13:15:04 +1200

    fix 1

--- sources/example.cpp
+++ sources/example.cpp
@@ -1,8 +1,7 @@
 // Copyright 2022 Evgenzayts evgenzaytsev2002@yandex.ru
 
 #include <stdexcept>
-
-#include <example.hpp>
+#include "example.hpp"
 
 auto example() -> void {
   throw std::runtime_error("not implemented");
