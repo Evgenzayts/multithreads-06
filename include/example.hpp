f4f3849510bcd9c69b42a36c27b9fbc94efc1b53
Evgenzayts <evgenzaytsev2002@yandex.ru>
2022-05-15 13:36:21 +1200

    fix 2

--- include/example.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 Evgenzayts evgenzaytsev2002@yandex.ru
-
-#ifndef INCLUDE_EXAMPLE_HPP_
-#define INCLUDE_EXAMPLE_HPP_
-
-auto example() -> void;
-
-#endif // INCLUDE_EXAMPLE_HPP_
--- sources/example.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 Evgenzayts evgenzaytsev2002@yandex.ru
-
-#include <stdexcept>
-#include "example.hpp"
-
-auto example() -> void {
-  throw std::runtime_error("not implemented");
-}
